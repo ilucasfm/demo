27e9a759f93af10c9ace59cad40bb385375e2e7e
Lucas Fernandes <ilucasfm@hotmail.com>
2023-04-06 13:27:13 -0300

    criado script

---
+++ app.js
@@ -0,0 +1,13 @@
+document
+ .querySelector("body > button:nth-child(2)")
+ .addEventListener("click", () => {
+  console.log("oi");
+  document.querySelector("body > h1").textContent = "Olá lucão 🔥";
+ });
+
+document
+ .querySelector("body > button:nth-child(3)")
+ .addEventListener("click", () => {
+  console.log("oi");
+  document.querySelector("body > h1").textContent = "Hello lucão 🔥";
+ });
